c5e18ce7ef46493ea2cbdc03048d36a80b6c9047
franzosterkamp <franz.osterkamp@gmx.net>
2019-12-18 14:43:31 +0100

    Add main theme

---
+++ client/src/theme/defaultTheme.js
@@ -0,0 +1,11 @@
+const main = {
+  colors: {
+    background: '#FFF',
+    primary: '#F2AD2D',
+    secondary: '#FF7272',
+    special: '##D7F4FF',
+    text: '#F0F0F0'
+  }
+};
+
+export default main;
