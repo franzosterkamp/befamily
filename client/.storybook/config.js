f5cf9d420b30df2e06a1fd112c01a81932213b84
franzosterkamp <franz.osterkamp@gmx.net>
2019-12-18 11:00:05 +0100

    Finalize storybook

--- client/.storybook/config.js
+++ client/.storybook/config.js
@@ -1,4 +1,6 @@
-import { configure } from '@storybook/react';
+import { configure, addDecorator } from "@storybook/react";
+import { withKnobs } from "@storybook/addon-knobs";
 
+addDecorator(withKnobs);
 // automatically import all files ending in *.stories.js
-configure(require.context('../src/stories', true, /\.stories\.js$/), module);
+configure(require.context("../src/stories", true, /\.stories\.js$/), module);
