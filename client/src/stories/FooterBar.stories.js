5dd3063854895d951a20984d98ad1408fb3b937c
franzosterkamp <franz.osterkamp@gmx.net>
2019-12-19 10:42:52 +0100

    Add FooterBar with 3 Buttons for Sort Add and Menu

---
+++ client/src/stories/FooterBar.stories.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import FooterBar from '../components/Footer/FooterBar';
+
+export default {
+  title: 'FooterBar'
+};
+
+export function Footer() {
+  return <FooterBar></FooterBar>;
+}
--- client/src/stories/HeaderNav.stories.js
+++ client/src/stories/HeaderNav.stories.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import HeaderNav from '../components/HeaderNav';
+import HeaderNav from '../components/Header/HeaderNav';
 
 export default {
   title: 'HeaderNav'
