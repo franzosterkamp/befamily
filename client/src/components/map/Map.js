c4047e08cff36d252ca136bbb6dd0fc0d20cc095
franzosterkamp <franz.osterkamp@gmx.net>
2019-12-20 16:16:03 +0100

    update map function and update satellite button

--- client/src/components/map/Map.js
+++ client/src/components/map/Map.js
@@ -1,49 +1,66 @@
 import React from 'react';
 import styled from '@emotion/styled';
 import mapboxgl from 'mapbox-gl';
-import Button from '../general/Button';
+import SatelliteIcon from '../../icons/Satellite';
 
 const MapContainer = styled.div`
   width: 500px;
   height: 500px;
+  position: absolute;
 `;
 
 mapboxgl.accessToken =
   'pk.eyJ1IjoiZnJhbno4NiIsImEiOiJjazRkeTF5b20wNTdoM2tuNmU5eG1kbDdqIn0.a-iKeML6h5r1j51BuXjvuA';
 
-export default function Map(onClick) {
+export default function Map() {
   const [lng, setLng] = React.useState(6.960279);
   const [lat, setLat] = React.useState(50.937531);
   const [zoom, setZoom] = React.useState(11);
   const [mapStyle, setMapStyle] = React.useState('mapbox://styles/mapbox/streets-v11');
 
-  function handleClickMap() {
-    setMapStyle('mapbox://styles/mapbox/streets-v11');
-    // setLng(map.getCenter().lng.toFixed(4));
-    // setLat(map.getCenter().lat.toFixed(4));
-    // setZoom(map.getZoom().toFixed(2));
-  }
+  const MapButton = styled.button`
+    z-index: 1;
+    margin: 15px;
+    border: none;
+    outline: none;
+    padding: 0px;
+    border-radius: 5px;
+    position: absolute;
+    background-color: transparent;
+    fill: ${mapStyle === 'mapbox://styles/mapbox/streets-v11'
+      ? props => props.theme.colors.rate
+      : props => props.theme.colors.special};
+  `;
 
-  function handleClickSatellite() {
-    setMapStyle('mapbox://styles/mapbox/satellite-v9');
-    // setLng(map.getCenter().lng.toFixed(4));
-    // setLat(map.getCenter().lat.toFixed(4));
-    // setZoom(map.getZoom().toFixed(2));
+  const mapData = {
+    container: 'map_container',
+    style: mapStyle,
+    center: [lng, lat],
+    zoom: zoom
+  };
+
+  function handleClickMap() {
+    if (mapStyle === 'mapbox://styles/mapbox/streets-v11') {
+      setMapStyle('mapbox://styles/mapbox/satellite-v9');
+    } else {
+      setMapStyle('mapbox://styles/mapbox/streets-v11');
+    }
   }
 
   React.useEffect(() => {
-    const map = new mapboxgl.Map({
-      container: 'map_container',
-      style: mapStyle,
-      center: [lng, lat],
-      zoom: zoom
+    const map = new mapboxgl.Map(mapData);
+    map.on('move', () => {
+      setLng(map.getCenter().lng.toFixed(4));
+      setLat(map.getCenter().lat.toFixed(4));
+      setZoom(map.getZoom().toFixed(2));
     });
   }, [mapStyle]);
 
   return (
     <MapContainer id="map_container">
-      <Button onClick={handleClickMap}>Map</Button>
-      <Button onClick={handleClickSatellite}>Satellite</Button>
+      <MapButton onClick={handleClickMap}>
+        <SatelliteIcon />
+      </MapButton>
     </MapContainer>
   );
 }
