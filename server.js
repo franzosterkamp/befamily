2a0ca1222552e9fdbc96dde4706ed2eaadcedf45
franzosterkamp <franz.osterkamp@gmx.net>
2020-01-07 11:46:42 +0100

    add backend part for detailpage

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const { dbInit } = require('./lib/database');
-const { getPlaces, setPlaces } = require('./lib/places');
+const { getPlaces, setPlaces, getPlaceById } = require('./lib/places');
 const app = express();
 const db = process.env.DB_NAME;
 const url = process.env.DB_URL;
@@ -16,6 +16,17 @@ app.get('/api/places', async (req, res) => {
   } catch (error) {
     console.error(error);
     res.send(error);
+  }
+});
+
+app.get('/api/places/:placeId', async (req, res) => {
+  try {
+    const placeId = req.params.placeId;
+    const place = await getPlaceById(placeId);
+    res.send(place);
+  } catch (error) {
+    console.error(error);
+    return res.status(404).end('Error');
   }
 });
 
