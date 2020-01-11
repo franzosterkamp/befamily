8117a4a2eeb8326486c91bd582780b8c2bf3ab99
Franz Osterkamp <56834039+franzosterkamp@users.noreply.github.com>
2020-01-11 13:30:51 +0000

    restyle of header (#51)

--- client/src/components/Header/HeaderButton.js
+++ client/src/components/Header/HeaderButton.js
@@ -1,36 +1,21 @@
 import styled from '@emotion/styled';
 import { Link } from 'react-router-dom';
 
-export const MapButton = styled(Link)`
+export const Button = styled(Link)`
   text-align: center;
-  width: 52%;
-  height: 100%;
+  width: 40%;
+  height: 75%;
   text-transform: uppercase;
-  border-bottom: solid 3px
+  border-radius: 7px;
+  border: solid 2px
     ${props => (props.active ? props.theme.colors.primary : props.theme.colors.text)};
   outline: none;
-  font-size: 1rem;
+  font-size: 0.7rem;
   font-weight: bold;
   text-decoration: none;
+  letter-spacing: 0.1rem;
   color: ${props => (props.active ? props.theme.colors.primary : props.theme.colors.text)};
   background-color: ${props =>
     props.active ? props.theme.colors.text : props.theme.colors.prmiary};
   fill: ${props => (props.active ? props.theme.colors.primary : props.theme.colors.text)};
-`;
-
-export const ListButton = styled(Link)`
-  fill: ${props => (props.active ? props.theme.colors.primary : props.theme.colors.text)};
-  text-align: center;
-  width: 52%;
-  height: 100%;
-  text-transform: uppercase;
-  border-bottom: solid 3px
-    ${props => (props.active ? props.theme.colors.primary : props.theme.colors.text)};
-  font-weight: bold;
-  outline: none;
-  font-size: 1rem;
-  text-decoration: none;
-  color: ${props => (props.active ? props.theme.colors.primary : props.theme.colors.text)};
-  background-color: ${props =>
-    props.active ? props.theme.colors.text : props.theme.colors.prmiary};
 `;
