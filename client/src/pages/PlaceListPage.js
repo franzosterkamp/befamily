3c3ee322f84448922a70c4b701ffd7cb7673301a
franzosterkamp <franz.osterkamp@gmx.net>
2020-01-13 12:42:32 +0100

    change for heroku

--- client/src/pages/PlaceListPage.js
+++ client/src/pages/PlaceListPage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Card from '../components/card/Card';
+import Card from '../components/Card/Card';
 import { PlacesContainer } from '../components/General/Container';
 import PropTypes from 'prop-types';
 
